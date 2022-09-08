9d44a31e1d41216d296e6cd1d94d97256d2622ea
EDUARDO ONETTO <EDUARDO.ONETTO97@GMAIL.COM>
2022-09-07 21:25:46 -0400

    Debido a Captcha cambiamos scraping a otro site

--- rutificador.py
+++ rutificador.py
@@ -1,7 +1,8 @@
+# @author @eduardoonetto https://github.com/eduardoonetto/Rutificador_Chile_Python
+
 import requests,urllib3, sys, locale
 from bs4 import BeautifulSoup
-
-#Uso python rutificador.py [19746549-2 || 19746549 || 19.746.549-2 || 19.746.549]
+import json
 
 def limpia_guion(Run):
     if int(Run.find("-")) > 0:
@@ -18,57 +19,48 @@ def coloca_puntos(Run):
     Run = locale.format_string('%d', int(Run), 1)
     return Run    
 
-def envia_y_limpia(data):
+def envia_y_limpia(rut):
     urllib3.disable_warnings()
     s=requests.Session()
     s.headers.update(headers)
-    r=s.post(URL, data=data)
+    r=s.post(URL+rut)
     soup=BeautifulSoup(r.content,"html.parser")
-    #print(soup)
     data = []
-    table = soup.find('table')
-    #print(table)
-    table_body = table.findAll('tr')[1]
-    #print(table_body)
+    table_body = soup.find('tr')
     data = str(table_body.findAll('td'))
-    data = data.replace("<tr>", '')
-    data = data.replace("</tr>", '')
-    data = data.replace("</td>", '')
     data = data.replace("<td>", '')
+    data = data.replace("</td>", '')
     data = data.replace("[", '')
     data = data.replace("]", '')
     data = data.split(",")
     return data
 try:
     #Envio POST a:
-    URL = "https://rutificador.org/backend.php"
-    # Necesito estos datos
-    head = ["Run", "Nombre", "Sexo", "Direccion Servel", "Ciudad"]
+    URL = "https://r.rutificador.co/pr/"
 
-    run_origen = None
+    # Get args from command line
     Run=sys.argv[1]
-    
+
     headers={
         "Content-Type" : "application/x-www-form-urlencoded",
         "User-Agent" : "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36"
-            }
+    }
 
-    #guardar dato sin formatear
-    run_origen = Run
-    Run = limpia_guion(Run)
-    Run = coloca_puntos(Run)
+    # Web scraping rutificador
+    data = envia_y_limpia(Run)
 
-    data={
-        "rut": Run,
-        "action": "search_by_rut"
-        }
-
-    data = envia_y_limpia(data)
-
+    # Necesito estos datos
+    keys = ["nombre", "run", "sexo", "direccion_servel", "circunscripcion_electoral", "comuna"]
+    # Recorrer datos y transformarlos a diccionario
+    datos_servel = {}
     count = 0
-    for h in head:
-        print(f"{h} : {data[count]}")
+    for k in keys:
+        datos_servel[k] = str(data[count]).strip()
         count += 1
+
+    # Datos a devolver
+    print(json.dumps(datos_servel))
+
 except NameError:
     print(f"Uso: python rutificador.py [00000000-0 || 00000000 || 00.000.000-0 || 00.000.000]")
 except IndexError:
